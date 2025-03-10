9446c9154e18d911f6cab02466e2be20c3973ec4
Ricardo Alexandre <you@example.com>
2025-03-10 10:49:40 -0300

    lideranca

--- server.js
+++ server.js
@@ -7,24 +7,29 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Configuração do EJS como motor de visualização
-app.set('views', path.join(__dirname, 'views')); // Caminho para a pasta views
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Rotas
 app.get('/', (req, res) => {
-    res.render('index'); // Renderiza o arquivo index.ejs
+    res.render('index'); // Renderiza o arquivo views/index.ejs
 });
 
 app.get('/sobre', (req, res) => {
-    res.render('pages/sobre'); // Renderiza o arquivo sobre.ejs
+    res.render('pages/sobre'); // Renderiza o arquivo views/pages/sobre.ejs
 });
 
 app.get('/contato', (req, res) => {
-    res.render('pages/contato'); // Renderiza o arquivo contato.ejs
+    res.render('pages/contato'); // Renderiza o arquivo views/pages/contato.ejs
 });
 
 app.get('/teste', (req, res) => {
-    res.render('pages/teste'); // Renderiza o arquivo teste.ejs
+    res.render('pages/teste'); // Renderiza o arquivo views/pages/teste.ejs
+});
+
+// Rota para lidar com erros 404 (página não encontrada)
+app.use((req, res) => {
+    res.status(404).render('pages/404'); // Renderiza uma página de erro 404 (opcional)
 });
 
 // Iniciar o servidor
